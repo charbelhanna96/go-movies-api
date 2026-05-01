9c807fe5fb15f63b8972386b1b4f0de2ed7ef8d2
Charbel Hanna <charbelhanna1996@gmail.com>
2026-05-01 14:25:52 -0400

    add movie and director models

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -1,1 +1,23 @@
+// Models for the movie entity.
 package model
+
+type Director struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type Genre struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+type Movie struct {
+	ID           int      `json:"id"`
+	Title        string   `json:"title"`
+	YearReleased int      `json:"yearReleased"`
+	Rating       float64  `json:"rating"`
+	DurationMins int      `json:"durationMins"`
+	Genre        Genre    `json:"genre"`
+	Director     Director `json:"director"`
+}
